bbaa099e3b3f28b6d5f6ac8a9443eea02057168e
GeuberLucas <geuberlu@gmail.com>
2021-05-21 20:00:00 -0300

    Hello world em java

--- java/hello word/src/App.java
+++ java/hello word/src/App.java
@@ -1,5 +1,7 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args)  {
+        var nome="Geuber";
+        System.out.println("Hello, World!"+nome);
+
     }
 }
